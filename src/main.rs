d1bdabbbef07b27cdee211b257a54135f172b6ab
Adhish Sunishkumar Cherukattil <a.sunishkumarcherukattil@se25.qmul.ac.uk>
2026-07-25 21:22:18 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process;
+
 fn main() {	
+
     loop {
+
     print!("$ ");
     io::stdout().flush().unwrap();
+
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
+
     command = command.trim().to_string();
+
+    if command.starts_with("echo ") {
+    println!("{}",command);
+    }
     if command == "exit" {
     process::exit(0);
+
     }
+
     println!("{}: command not found", command.trim());
 }
 }
