99076024862190eae484f03d6848e794536c63ac
Adhish Sunishkumar Cherukattil <a.sunishkumarcherukattil@se25.qmul.ac.uk>
2026-07-25 20:53:36 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+fn main() {	
+    loop {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
     println!("{}: command not found", command.trim());
+}
 }
