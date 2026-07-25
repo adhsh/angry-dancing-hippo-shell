1b3bf3a123af1fc00f6c9211d4a31f80991c31f6
Adhish Sunishkumar Cherukattil <a.sunishkumarcherukattil@se25.qmul.ac.uk>
2026-07-25 21:05:29 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut command = String::new();
+    command = command.trim().to_string();
     io::stdin().read_line(&mut command).unwrap();
+    if command == "exit" {
+    process::exit(0);
+    }
     println!("{}: command not found", command.trim());
 }
 }
