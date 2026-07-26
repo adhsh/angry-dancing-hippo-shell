f19441d99ddd2747f49426443cd3eae1e348f8fb
Adhish Sunishkumar Cherukattil <a.sunishkumarcherukattil@se25.qmul.ac.uk>
2026-07-26 13:34:41 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,31 @@
 use std::io::{self, Write};
 use std::process;
 
-fn main() {	
-
+fn main() {
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
 
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
+        command = command.trim().to_string();
 
-    command = command.trim().to_string();
-	
-    if command.starts_with("type ") {
-    let arg = &command[5..];
-    let builtins = ["echo", "exit", "type"];
+        if command == "exit 0" {
+            std::process::exit(0);
+        } else if command.starts_with("echo ") {
+            println!("{}", &command[5..]);
+       } else if command.starts_with("type ") {
+            let arg = &command[5..];
+            let builtins = ["echo", "exit", "type"];
 
-    if builtins.contains(&arg) {
-	println!("{} is a shell builtin", arg);
-    } else {
-	println!("{}: not found", arg)
+            if builtins.contains(&arg) {
+                println!("{} is a shell builtin", arg);
+            } else {
+                println!("{}: not found", arg);
+            }
+        } else {
+            println!("{}: command not found", command);
+        }
     }
-    }
-
-}
 }
