551d4cc19750a9edd93dba5d8f1e4471bfb1aa91
Adhish Sunishkumar Cherukattil <a.sunishkumarcherukattil@se25.qmul.ac.uk>
2026-07-26 13:54:26 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,31 @@ fn main() {
             std::process::exit(0);
         } else if command.starts_with("echo ") {
             println!("{}", &command[5..]);
-       } else if command.starts_with("type ") {
+        } else if command.starts_with("type ") {
             let arg = &command[5..];
             let builtins = ["echo", "exit", "type"];
 
             if builtins.contains(&arg) {
                 println!("{} is a shell builtin", arg);
             } else {
-                println!("{}: not found", arg);
-            }
-        } else {
-            println!("{}: command not found", command);
-        }
-    }
-}
+                let path = std::env::var("PATH").unwrap_or_default();
+                let mut found = false;
+                for dir in path.split(':') {
+                    let full_path = format!("{}/{}", dir, arg);
+                    let path_obj = std::path::Path::new(&full_path);
+                    if path_obj.exists() {
+                        use std::os::unix::fs::PermissionsExt;
+                        if let Ok(metadata) = std::fs::metadata(&full_path) {
+                            if metadata.permissions().mode() & 0o111 != 0 {
+                                println!("{} is {}", arg, full_path);
+                                found = true;
+                                break;
+                      }
+                  }
+              }
+          }
+          if !found {
+              println!("{}: not found", arg);
+          }
+      }
+  }
